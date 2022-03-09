105f9be9d0340dfdecd3027041711fa16511b4f5
Jason Gunawan <55174646+jasandgun@users.noreply.github.com>
2022-03-09 10:36:03 +0700

    init spring

---
+++ src/main/java/com/hackathon/hackalhot/HackalhotApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.hackalhot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackalhotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackalhotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/hackalhot/HackalhotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.hackalhot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackalhotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
